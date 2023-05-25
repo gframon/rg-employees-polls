93bb7621003f0437de86e98e20e22a36d8c13841
Ramon Gomez <ramon.gomez@noaa.gov>
2023-05-25 17:39:56 -0400

    Implemented Tabs on Dashboard.

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,46 +1,81 @@
+import { useState } from "react";
 import { connect } from "react-redux";
 import CssBaseline from "@mui/material/CssBaseline";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
-import { Divider } from "@mui/material";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 import Question from "./Question";
 
 const Dashboard = (props) => {
+  const [value, setValue] = useState("new");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <>
       <CssBaseline />
       <Container maxWidth="xl">
         <Box>
-          <ul>
-            {Object.keys(props.sections).map((key, idx) => (
+          <Box
+            sx={{
+              width: "100%",
+              border: "solid 1px #ACACAC",
+              borderRadius: "5px",
+              marginTop: "20px",
+            }}
+          >
+            <Tabs
+              orientation="horizontal"
+              value={value}
+              onChange={handleChange}
+              sx={{ borderRight: 1, borderColor: "divider" }}
+              centered
+            >
+              <Tab value={"new"} label="New Questions" />
+              <Tab value={"done"} label="Done" />
+            </Tabs>
+          </Box>
+          <Box
+            sx={{
+              padding: 2,
+              border: "solid 1px #ACACAC",
+              borderRadius: "5px",
+              marginTop: "20px",
+            }}
+          >
+            {value === "new" && (
               <Box
-                key={idx}
-                style={{
-                  border: "solid 1px #ACACAC",
-                  borderRadius: "5px",
-                  marginTop: "20px",
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  flexWrap: "wrap",
+                  alignContent: "space-between",
+                  margin: "10px",
                 }}
               >
-                <h2 className="center">
-                  {key.startsWith("new") ? "New Questions" : "Done"}
-                </h2>
-                <Divider />
-                <Box
-                  sx={{
-                    display: "flex",
-                    flexDirection: "row",
-                    flexWrap: "wrap",
-                    alignContent: "space-between",
-                    margin: "10px",
-                  }}
-                >
-                  {props.sections[key].map((id) => (
-                    <Question key={id} id={id} />
-                  ))}
-                </Box>
+                {props.sections[value].map((id) => (
+                  <Question key={id} id={id} />
+                ))}
               </Box>
-            ))}
-          </ul>
+            )}
+            {value === "done" && (
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "row",
+                  flexWrap: "wrap",
+                  alignContent: "space-between",
+                  margin: "10px",
+                }}
+              >
+                {props.sections[value].map((id) => (
+                  <Question key={id} id={id} />
+                ))}
+              </Box>
+            )}
+          </Box>
         </Box>
       </Container>
     </>
@@ -49,15 +84,15 @@ const Dashboard = (props) => {
 
 const mapStateToProps = (state) => {
   const { questions, authedUser } = state;
-  let dashboard = { newQuestions: [], doneQuestions: [] };
+  let dashboard = { new: [], done: [] };
   Object.values(questions)
     .sort((a, b) => b.timestamp - a.timestamp)
     .map(({ id, optionOne, optionTwo }) => {
       return optionOne.votes.includes(authedUser) || optionTwo.votes.includes(authedUser)
-        ? dashboard.doneQuestions.push(id)
-        : dashboard.newQuestions.push(id);
+        ? dashboard.done.push(id)
+        : dashboard.new.push(id);
     });
-    
+
   return {
     sections: { ...dashboard },
   };
